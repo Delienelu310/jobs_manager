f4837c2164cdb6ef435896d6bda3d9759e5cff4e
Delienelu310 <seredoha.nets@gmail.com>
2024-04-21 16:43:19 +0200

    Iteration 1: Initialize manager spring boot app

---
+++ jobs_manager/src/main/java/com/ilumusecase/jobs_manager/JobsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ilumusecase.jobs_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsManagerApplication.class, args);
+	}
+
+}
---
+++ jobs_manager/src/test/java/com/ilumusecase/jobs_manager/JobsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilumusecase.jobs_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
