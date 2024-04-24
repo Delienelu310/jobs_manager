c9ec3c22686a4dd1355cbd472e2d785fdf0847a5
Delienelu310 <seredoha.nets@gmail.com>
2024-04-24 16:24:58 +0200

    Iteration 1: Add some basic request handlers for job_node

--- jobs_manager/src/main/java/com/ilumusecase/jobs_manager/controllers/JobsNodeController.java
+++ jobs_manager/src/main/java/com/ilumusecase/jobs_manager/controllers/JobsNodeController.java
@@ -6,10 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ilumusecase.jobs_manager.json_mappers.JsonMappersFactory;
 import com.ilumusecase.jobs_manager.repositories.interfaces.RepositoryFactory;
+import com.ilumusecase.jobs_manager.resources.JobNode;
+import com.ilumusecase.jobs_manager.resources.JobNodeDetails;
 
 
 @RestController
@@ -22,12 +27,42 @@ public class JobsNodeController {
 
     @GetMapping("/job_nodes")
     public MappingJacksonValue retrieveAllNodes(){
-        return null;
+        return jsonMappersFactory.getJobNodeJsonMapper().getFullJobNodeList(
+            repositoryFactory.getJobNodesRepository().retrieveAll()
+        );
     }
 
-    @GetMapping("/projects/{project_id/job_nodes/{job_node_id}}")
+    @GetMapping("/projects/{project_id}/job_nodes/{job_node_id}}")
     public MappingJacksonValue retrieveById(@PathVariable("project_id") String projectId, @PathVariable("job_node_id") String jobNodeId){
-        return null;   
+        return jsonMappersFactory.getJobNodeJsonMapper().getFullJobNode( 
+            repositoryFactory.getJobNodesRepository().retrieveById(jobNodeId)
+        );
     }
+
+    @GetMapping("/projects/{project_id}/job_nodes")
+    public MappingJacksonValue retrieveJobNodesByProjectId(@PathVariable("project_id") String projectId){
+        return jsonMappersFactory.getJobNodeJsonMapper().getFullJobNodeList(
+            repositoryFactory.getJobNodesRepository().retrieveByProjectId(projectId)
+        );
+    }
+
+    @PostMapping("/projects/{project_id}/job_nodes")
+    public MappingJacksonValue createJobNode(@PathVariable("project_id") String projectId, @RequestBody JobNodeDetails jobNodeDetails){
+        return jsonMappersFactory.getJobNodeJsonMapper().getFullJobNode(
+            repositoryFactory.getJobNodesRepository().createJobNode(
+                repositoryFactory.getProjectRepository().retrieveProjectById(projectId), 
+                jobNodeDetails
+            )
+        );
+    }
+
+    @PutMapping("/projects/{project_id}/job_nodes/{job_node_id}")
+    public MappingJacksonValue updateJobNodeDetails(@PathVariable("job_node_id") String jobNodeId, @RequestBody JobNodeDetails jobNodeDetails){
+        return jsonMappersFactory.getJobNodeJsonMapper().getFullJobNode(
+            repositoryFactory.getJobNodesRepository().updateJobNode(jobNodeId, jobNodeDetails)
+        );
+    }
+
+    
    
 }
